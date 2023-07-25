84a35cb15fed58bde221ca914c91f7293c91b900
Stepan Belenov <Sabaneev99@yandex.ru>
2023-07-25 14:49:49 +0300

    Loggers

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,8 @@ const { auth } = require('./middlewares/auth');
 
 const { cors } = require('./middlewares/cors');
 
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+
 const { login, addUser } = require('./controllers/users');
 
 const { validatorLogin, validatorAddUser } = require('./middlewares/validator');
@@ -24,10 +26,13 @@ const NotDataError = require('./errors/NotDataError');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+
 app.use(cors);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(requestLogger);
 
 app.post('/signin', validatorLogin, login);
 app.post('/signup', validatorAddUser, addUser);
@@ -37,7 +42,10 @@ app.use(auth);
 app.use('/users', userRoutes);
 app.use('/cards', cardsRoutes);
 
+app.use(errorLogger);
+
 app.use(errors());
+
 
 app.use('', (req, res, next) => {
   next(new NotDataError('Данного пути не существует'));
