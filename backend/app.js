b6701ba5ac25f80da96f2c4592961ced30289bff
Stepan Belenov <Sabaneev99@yandex.ru>
2023-07-25 14:56:43 +0300

    Loggers

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(requestLogger);
+//app.use(requestLogger);
 
 app.post('/signin', validatorLogin, login);
 app.post('/signup', validatorAddUser, addUser);
