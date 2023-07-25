d142a712406f5aecd399348b243c97a831f24ff0
Stepan Belenov <Sabaneev99@yandex.ru>
2023-07-25 12:57:45 +0300

    замена BASE_URL

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -5,7 +5,7 @@ function checkResponse(res) {
   return Promise.reject(res.status);
 }
 
-export const BASE_URL = "https://auth.nomoreparties.co";
+export const BASE_URL = "https://api.mestostepan.nomoredomains.xyz";
 
 export function registerNewUser(email, password) {
   return fetch(`${BASE_URL}/signup`, {
