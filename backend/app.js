f3777246cd792a4550af7586f80c936e6f68ad70
Stepan Belenov <Sabaneev99@yandex.ru>
2023-07-25 14:59:41 +0300

    Откат

--- backend/app.js
+++ backend/app.js
@@ -26,13 +26,10 @@ const NotDataError = require('./errors/NotDataError');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-
 app.use(cors);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//app.use(requestLogger);
 
 app.post('/signin', validatorLogin, login);
 app.post('/signup', validatorAddUser, addUser);
@@ -42,10 +39,7 @@ app.use(auth);
 app.use('/users', userRoutes);
 app.use('/cards', cardsRoutes);
 
-//app.use(errorLogger);
-
 app.use(errors());
-
 
 app.use('', (req, res, next) => {
   next(new NotDataError('Данного пути не существует'));
