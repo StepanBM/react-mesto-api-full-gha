b2b0b0a304be7910cb98a4eb572733e568849d40
Stepan Belenov <Sabaneev99@yandex.ru>
2023-07-26 10:25:17 +0300

    Loggers

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,10 @@ const NotDataError = require('./errors/NotDataError');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(requestLogger);
+
 app.use(cors);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,6 +42,8 @@ app.use(auth);
 app.use('/users', userRoutes);
 app.use('/cards', cardsRoutes);
 
+app.use(errorLogger);
+
 app.use(errors());
 
 app.use('', (req, res, next) => {
@@ -46,6 +51,12 @@ app.use('', (req, res, next) => {
 });
 
 app.use(serverError);
+
+/* app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+}); */
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
